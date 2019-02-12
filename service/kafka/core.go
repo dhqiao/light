1e52e3ed9b8af07f639a523a874b82dce456b720
qiaodahai735 <qiaodahai735@git.ipo.com>
2019-02-12 13:50:48 +0800

    update async

--- service/kafka/core.go
+++ service/kafka/core.go
@@ -11,28 +11,14 @@ var Address = []string{"127.0.0.1:9092","127.0.0.1:9091","127.0.0.1:9094"}
 
 // 异步模式
 func SaramaProducer() {
-
-	config := sarama.NewConfig()
-	//等待服务器所有副本都保存成功后的响应
-	config.Producer.RequiredAcks = sarama.WaitForAll
-	//随机向partition发送消息
-	config.Producer.Partitioner = sarama.NewRandomPartitioner
-	//是否等待成功和失败后的响应,只有上面的RequireAcks设置不是NoReponse这里才有用.
-	config.Producer.Return.Successes = true
-	config.Producer.Return.Errors = true
-	//设置使用的kafka版本,如果低于V0_10_0_0版本,消息中的timestrap没有作用.需要消费和生产同时配置
-	//注意，版本设置不对的话，kafka会返回很奇怪的错误，并且无法成功发送消息
-	config.Version = sarama.V2_1_0_0
-
-	fmt.Println("start make producer")
-	//使用配置,新建一个异步生产者
-	producer, e := sarama.NewAsyncProducer(Address, config)
+	producer, e := NewAsyncProducer()
 	if e != nil {
 		fmt.Println(e)
 		return
 	}
 	defer producer.AsyncClose()
 
+	/*
 	//循环判断哪个通道发送过来数据.
 	fmt.Println("start goroutine")
 	go func(p sarama.AsyncProducer) {
@@ -44,7 +30,7 @@ func SaramaProducer() {
 				fmt.Println("err: ", fail.Err)
 			}
 		}
-	}(producer)
+	}(producer)*/
 
 	var value string
 	for i := 0; ; i++ {
@@ -60,10 +46,27 @@ func SaramaProducer() {
 
 		//将字符串转化为字节数组
 		msg.Value = sarama.ByteEncoder(value)
-		//fmt.Println(value)
 
 		//使用通道发送
 		producer.Input() <- msg
 	}
 
+}
+
+func NewAsyncProducer() (sarama.AsyncProducer, error) {
+	config := sarama.NewConfig()
+	//等待服务器所有副本都保存成功后的响应
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	//随机向partition发送消息
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	//是否等待成功和失败后的响应,只有上面的RequireAcks设置不是NoReponse这里才有用.
+	config.Producer.Return.Successes = true
+	config.Producer.Return.Errors = true
+	//设置使用的kafka版本,如果低于V0_10_0_0版本,消息中的timestrap没有作用.需要消费和生产同时配置
+	//注意，版本设置不对的话，kafka会返回很奇怪的错误，并且无法成功发送消息
+	config.Version = sarama.V2_1_0_0
+
+	fmt.Println("start make producer")
+	//使用配置,新建一个异步生产者
+	return sarama.NewAsyncProducer(Address, config)
 }
